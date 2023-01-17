da1e59108933c40114a746f0ef46d4a7935b472c
hasan <hasanelmi678@gmail.com>
2023-01-17 15:24:30 +0000

    created wave portal

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,27 +1,197 @@
 import { ethers } from "ethers";
+import { useEffect, useState } from 'react';
+import abi from '../utils/WavePortal.json';
 
 export default function Home() {
+  const getEthereumObject = () => window.ethereum;
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [waveCount, setWaveCount] = useState(null);
+  const [allWaves, setAllWaves] = useState([]);
 
-  const wave = () => {
 
+  const contractAddress = "0xB10b4BdE5C1EF034817f1778277100A540aF89Cf";
+  const contractABI = abi.abi;
+
+  const findMetaMaskAccount = async () => {
+    try {
+      const ethereum = getEthereumObject();
+      if (!ethereum) {
+        console.log("Make sure you have MetaMask.");
+        return null;
+      }
+
+      console.log("We have the ethereum object: ", ethereum);
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+      } else {
+        console.error("No authorized account found.");
+        return null;
+      }
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const ethereum = getEthereumObject();
+      if (!ethereum) {
+        alert("Download MetaMask to use this dApp.")
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+
+    } catch (error) {
+      console.error(error);
+    }
   }
-  
+
+
+
+  const getAllWaves = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // "Provider" is what we use to actually talk to Ethereum nodes.
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let allWaves = await wavePortalContract.getAllWaves();
+        console.log("allWaves: ", allWaves);
+
+        let wavesCleaned = [];
+        allWaves.forEach(wave => {
+          wavesCleaned.push({
+            address: wave.waver,
+            timestamp: new Date(wave.timestamp * 1000),
+            message: wave.message
+          });
+        });
+
+        setAllWaves(wavesCleaned);
+
+
+        await getTotalWaveCount();
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // "Provider" is what we use to actually talk to Ethereum nodes.
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+
+        /*
+        * Execute the actual wave from your smart contract
+        */
+        const waveTxn = await wavePortalContract.wave("Random message");
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+
+        await getAllWaves();
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getTotalWaveCount = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // "Provider" is what we use to actually talk to Ethereum nodes.
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getTotalWaves();
+        setWaveCount(count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    findMetaMaskAccount();
+    getAllWaves();
+  }, []);
+
+  useEffect(() => {
+    if (currentAccount !== "") console.log("ACCOUNT: ", currentAccount);
+  }, [currentAccount]);
+
   return (
     <div className="mainContainer">
 
-    <div className="dataContainer">
-      <div className="header">
-      👋 Hey there!
-      </div>
+      <div className="dataContainer">
+        <div className="header">
+          👋 Hey there!
+        </div>
 
-      <div className="bio">
-      I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
-      </div>
+        <div className="bio">
+          I am Hascoin? Connect your Ethereum wallet and wave at me!
+        </div>
 
-      <button className="waveButton" onClick={wave}>
-        Wave at Me
-      </button>
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
+
+        {waveCount !== null && (
+          <div className="header" style={{ marginTop: '20px' }}>
+            {waveCount} Total Wave
+          </div>
+        )}
+
+        {allWaves.map((wave, index) => {
+          return (
+            <div key={index} style={{ backgroundColor: "OldLace", marginTop: "16px", padding: "8px" }}>
+              <div>Address: {wave.address}</div>
+              <div>Time: {wave.timestamp.toString()}</div>
+              <div>Message: {wave.message}</div>
+            </div>)
+        })}
+      </div>
     </div>
-  </div>
   )
 }
--- hardhat/scripts/run.js
+++ hardhat/scripts/run.js
@@ -1,38 +1,26 @@
 const main = async () => {
-    // grabbed the wallet address of contract owner and I also grabbed a random wallet address and called it randomPerson
-    const [owner, randomPerson] = await hre.ethers.getSigners();
-
     // compile our contract and generate the necessary files we need to work with our contract under the artifacts directory.
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-
     // Hardhat will create a local Ethereum network for us, but just for this contract. Then, after the script completes it'll destroy that local network.
     const waveContract = await waveContractFactory.deploy();
-
     // wait until our contract is officially deployed to our local blockchain!
     await waveContract.deployed();
-
     // waveContract.address  will basically give us the address of the deployed contract. This address is how we can actually find our contract on the blockchain
     console.log(`Contract deployed to: ${waveContract.address}`);
+    
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+    console.log(waveCount.toNumber());
 
-    // see the address of the person deploying our contract
-    console.log("Contract deployed by:", owner.address);
+    let waveTxn = await waveContract.wave("A message!");
+    await waveTxn.wait(); // Wait for the transaction to be mined
 
-    // call the function to grab the # of total waves
-    await waveContract.getTotalWaves();
+    const [_, randomPerson] = await hre.ethers.getSigners();
+    waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
+    await waveTxn.wait(); // Wait for the transaction to be mined
 
-    // wave and wait for the transaction to finish
-    const waveTxn = await waveContract.wave();
-    await waveTxn.wait();
-
-    // call the function to grab the # of total waves
-    await waveContract.getTotalWaves();
-
-    // random person wave and wait for the transaction to finish
-    const secondWaveTxn = await waveContract.connect(randomPerson).wave();
-    await secondWaveTxn.wait();
-
-    // call the function to grab the # of total waves
-    await waveContract.getTotalWaves();
+    let allWaves = await waveContract.getAllWaves();
+    console.log(allWaves);
 };
 
 const runMain = async () => {
