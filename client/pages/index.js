9b487a2892a028be09395f726b50454d7278e8d8
hasan <hasanelmi678@gmail.com>
2023-01-17 11:54:50 +0000

    created wave portal smart contract

---
+++ client/pages/index.js
@@ -0,0 +1,27 @@
+import { ethers } from "ethers";
+
+export default function Home() {
+
+  const wave = () => {
+
+  }
+  
+  return (
+    <div className="mainContainer">
+
+    <div className="dataContainer">
+      <div className="header">
+      👋 Hey there!
+      </div>
+
+      <div className="bio">
+      I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+      </div>
+
+      <button className="waveButton" onClick={wave}>
+        Wave at Me
+      </button>
+    </div>
+  </div>
+  )
+}
